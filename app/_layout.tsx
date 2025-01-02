16b5b03dbb16a567fbb122981aed4d68319e7b82
evanbacon <bacon@expo.io>
2025-01-02 12:21:51 -0600

    wip

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -12,7 +12,7 @@ export default function Layout() {
           name="index"
           options={{
             headerRight: () => (
-              <Form.Link headerRight href="/account">
+              <Form.Link href="/account">
                 <Avatar />
               </Form.Link>
             ),
