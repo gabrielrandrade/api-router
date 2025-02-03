f76f5c8167875e9304fd1eb1bcdf14e0e2ebb366
evanbacon <bacon@expo.io>
2025-02-03 01:28:17 -0800

    Update app.json output configuration and modify ThemeProvider for improved theme handling

--- components/ui/ThemeProvider.tsx
+++ components/ui/ThemeProvider.tsx
@@ -1,37 +1,38 @@
-import * as AppleColors from "@bacons/apple-colors";
+// import * as AppleColors from "@bacons/apple-colors";
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider as RNTheme,
-  Theme,
 } from "@react-navigation/native";
 import { useColorScheme } from "react-native";
 
 // Use exact native P3 colors and equivalents on Android/web.
 // This lines up well with React Navigation.
-const BaconDefaultTheme: Theme = {
-  dark: false,
-  colors: {
-    primary: AppleColors.systemBlue,
-    background: AppleColors.systemGroupedBackground,
-    card: AppleColors.secondarySystemGroupedBackground,
-    text: AppleColors.label,
-    border: AppleColors.separator,
-    notification: AppleColors.systemRed,
-  },
-  fonts: DefaultTheme.fonts,
-};
+// const BaconDefaultTheme: Theme = {
+//   dark: false,
+//   colors: {
+//     primary: AppleColors.systemBlue,
+//     notification: AppleColors.systemRed,
+//     ...DefaultTheme.colors,
+//     // background: AppleColors.systemGroupedBackground,
+//     // card: AppleColors.secondarySystemGroupedBackground,
+//     // text: AppleColors.label,
+//     // border: AppleColors.separator,
+//   },
+//   fonts: DefaultTheme.fonts,
+// };
 
-const BaconDarkTheme: Theme = {
-  dark: true,
-  colors: {
-    ...BaconDefaultTheme.colors,
-  },
-  fonts: DarkTheme.fonts,
-};
+// const BaconDarkTheme: Theme = {
+//   dark: true,
+//   colors: {
+//     // ...BaconDefaultTheme.colors,
+//     ...DarkTheme.colors,
+//   },
+//   fonts: DarkTheme.fonts,
+// };
 
 export default function ThemeProvider(props: { children: React.ReactNode }) {
-  const colorScheme = useColorScheme();
+  const colorScheme = process.env.EXPO_OS === "web" ? "dark" : useColorScheme();
   return (
     <RNTheme
       // This isn't needed on iOS or web, but it's required on Android since the dynamic colors are broken
