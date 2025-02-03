1bd7cdc5798ac0e7cdc512cbd42ca874f4975f4e
evanbacon <bacon@expo.io>
2025-02-02 18:51:52 -0800

    Refactor tab implementation and add custom Tabs component with system image support

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,61 +1,26 @@
 import ThemeProvider from "@/components/ui/ThemeProvider";
-import { Tabs } from "expo-router";
 
-import { IconSymbol } from "@/components/ui/IconSymbol";
-import BlurTabBarBackground from "@/components/ui/TabBarBackground.ios";
-import { BottomTabBarButtonProps } from "@react-navigation/bottom-tabs";
-import { PlatformPressable } from "@react-navigation/elements";
-import * as Haptics from "expo-haptics";
-import { Platform } from "react-native";
+import Tabs from "@/components/ui/Tabs";
 
 export default function Layout() {
   return (
     <ThemeProvider>
-      <Tabs
-        screenOptions={{
-          headerShown: false,
-          tabBarButton: HapticTab,
-          tabBarBackground: BlurTabBarBackground,
-          tabBarStyle: Platform.select({
-            ios: {
-              // Use a transparent background on iOS to show the blur effect
-              position: "absolute",
-            },
-            default: {},
-          }),
-          // tabBarActiveTintColor: AC.systemBlue as any,
-        }}
-      >
+      <Tabs>
         <Tabs.Screen
           name="(index)"
+          systemImage="house"
           options={{
             title: "Home",
-            tabBarIcon: (props) => <IconSymbol {...props} name="house.fill" />,
           }}
         />
         <Tabs.Screen
           name="(info)"
+          systemImage="brain.fill"
           options={{
             title: "Info",
-            tabBarIcon: (props) => <IconSymbol {...props} name="brain.fill" />,
           }}
         />
       </Tabs>
     </ThemeProvider>
-  );
-}
-
-function HapticTab(props: BottomTabBarButtonProps) {
-  return (
-    <PlatformPressable
-      {...props}
-      onPressIn={(ev) => {
-        if (process.env.EXPO_OS === "ios") {
-          // Add a soft haptic feedback when pressing down on the tabs.
-          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-        }
-        props.onPressIn?.(ev);
-      }}
-    />
   );
 }
