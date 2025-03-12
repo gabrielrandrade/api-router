3c197643337a876a9016439e5f6391a076b82745
gabrielrandrade <gabrielrandradesp@hotmail.com>
2025-03-12 10:42:55 -0300

    create: post api

---
+++ app/api/post+api.ts
@@ -0,0 +1,19 @@
+export function GET(request: Request) {
+  const data = [
+    {
+      id: 1,
+      text: "Just launched my new app!",
+      isLiked: false,
+    },
+    {
+      id: 2,
+      text: "Working on some new Ui designs today",
+      isLiked: true,
+    },
+    {
+      id: 3,
+      text: "Morning run complete!",
+      isLiked: true,
+    },
+  ];
+}
