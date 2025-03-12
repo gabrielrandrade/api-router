b757d4182c5acd489f576f648d7f0251fb4eea53
gabrielrandrade <gabrielrandradesp@hotmail.com>
2025-03-12 09:24:40 -0300

    chore: create dummy content

--- app/(index,info)/index.tsx
+++ app/(index,info)/index.tsx
@@ -10,24 +10,63 @@ export default function Page() {
   return (
     <View style={{ flex: 1 }}>
       <Form.List navigationTitle="Home">
-        <Form.Section title="Create Post" footer="Create a post to share with your friends">
+        <Form.Section
+          title="Create Post"
+          footer="Create a post to share with your friends"
+        >
           <Form.HStack>
-            <TextInput placeholder="What's on your mind?" style={{
-              flexGrow: 1,
-              maxWidth: "80%",
-              borderWidth: 1,
-              borderColor: AC.separator,
-              borderRadius: 8,
-              padding: 12,
-              color: colorScheme === "dark" ? "white" : "black",
-            }}/>
+            <TextInput
+              placeholder="What's on your mind?"
+              style={{
+                flexGrow: 1,
+                maxWidth: "80%",
+                borderWidth: 1,
+                borderColor: AC.separator,
+                borderRadius: 8,
+                padding: 12,
+                color: colorScheme === "dark" ? "white" : "black",
+              }}
+            />
             <Form.Link href={"/(info)"}>
               {/*@ts-ignore*/}
-              <IconSymbol name="sparkle" color={AC.systemOrange}/>
+              <IconSymbol name="sparkle" color={AC.systemOrange} />
             </Form.Link>
           </Form.HStack>
+        </Form.Section>
+        <Form.Section title="Recent Posts">
+          {dummyPost.map((post) => (
+            <Form.Link 
+            href={"/two"} 
+            key={post.id} 
+            systemImage={post.isLiked ? "heart.fill" : "heart"}
+            style={{
+              overflow: "hidden",
+              flexShrink: 1,
+            }}
+            >
+              {post.text}
+            </Form.Link>
+          ))}
         </Form.Section>
       </Form.List>
     </View>
   );
 }
+
+const dummyPost = [
+  {
+    id: 1,
+    text: "Just launched my new app!",
+    isLiked: false,
+  },
+  {
+    id: 2,
+    text: "Working on some new Ui designs today",
+    isLiked: true,
+  },
+  {
+    id: 3,
+    text: "Morning run complete!",
+    isLiked: true,
+  },
+];
